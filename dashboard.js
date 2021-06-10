0e0c548e574ee1bccc4b069f7e04eb2aece0d315
Namish4Web <85440682+Namish4Web@users.noreply.github.com>
2021-06-10 22:42:51 +0530

    Create dashboard.js

---
+++ dashboard.js
@@ -0,0 +1,24 @@
+function check() {
+						firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      // User is signed in, see docs for a list of available properties
+      // https://firebase.google.com/docs/reference/js/firebase.User
+      var uid = user.uid; 
+    } else {
+    alert("Please login first");
+ window.location.href =
+ "https://surveyzilla-4-web.web.app/login.html";
+ 
+    }
+  });						
+	}	
+	
+	function logout() {
+	 console.log('logging out...');
+	 firebase.auth().signOut().then(() => {
+	  alert("Logout successful");
+	  window.location.href = "https://surveyzilla-4-web.web.app";
+	}).catch((error) => {
+	alert("An error occurred");
+	})
+	}
