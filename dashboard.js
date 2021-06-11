3c860c47ec9b8f0109c9cec2af0e3d47b3709b7c
Arup <abehera@vocera.com>
2021-06-11 21:48:27 +0530

    Toast message

--- dashboard.js
+++ dashboard.js
@@ -1,18 +1,23 @@
+
+function showSampleToast(params) {
+	$('.toast').toast('show');
+}
+
 function check() {
 						firebase.auth().onAuthStateChanged((user) => {
     if (user) {
       // User is signed in, see docs for a list of available properties
       // https://firebase.google.com/docs/reference/js/firebase.User
-      var uid = user.uid; 
+      var uid = user.uid;
     } else {
     alert("Please login first");
  window.location.href =
  "https://surveyzilla-4-web.web.app/login.html";
- 
+
     }
-  });						
-	}	
-	
+  });
+	}
+
 	function logout() {
 	 console.log('logging out...');
 	 firebase.auth().signOut().then(() => {
